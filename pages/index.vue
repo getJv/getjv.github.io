b6d5d97c964f59e40b617ebcdcffd9344652fd5b
Jhonatan Morais <jhonatanvinicius@gmail.com>
2020-10-25 20:56:52 -0300

    code fix

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,8 @@
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
       <div class="text-center">
-        <!-- <logo />
-        <vuetify-logo /> -->
+        <logo />
+        <vuetify-logo />
       </div>
       <v-card>
         <v-card-title class="headline">

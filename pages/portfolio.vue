94e64986c6af45c47e26d10fb851c055790f1a9e
Jhonatan Morais <jhonatanvinicius@gmail.com>
2020-10-28 21:20:03 -0300

    new release

--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -1,83 +1,136 @@
 <template>
-  <v-timeline align-top :dense="$vuetify.breakpoint.smAndDown">
-    <v-timeline-item
-      v-for="(item, i) in items"
-      :key="i"
-      :color="item.color"
-      :icon="item.icon"
-      fill-dot
-    >
-      <v-card :color="item.color"  dark>
-        <v-card-title class="headline">
-            <div class="title font-weight-light" >{{ item.name }}</div>
-        </v-card-title>
+  <div>
+    <v-overlay v-if="loading">
+      <v-row justify="center" align="center" class="mb-5">
+        <v-progress-circular indeterminate size="64"></v-progress-circular>
+      </v-row>
+      <v-row justify="center" align="center">
+        <span v-if="gitLoading"> Git Repositories loading ... </span>
+        <span v-else> Git Repositories ready! </span>
+      </v-row>
+    </v-overlay>
+    <v-timeline v-else align-top :dense="$vuetify.breakpoint.smAndDown">
+      <v-timeline-item
+        v-for="(item, i) in items"
+        :key="i"
+        :color="item.color"
+        :icon="item.icon"
+        fill-dot
+      >
+        <v-card>
+          <v-card-title :class="`${item.color}`">
+            <div class="title font-weight-bold white--text">
+              {{ item.name }}
+            </div>
+          </v-card-title>
+          <v-divider></v-divider>
+          <v-card-subtitle>
+            created on {{ item.date | formatDate }}
+          </v-card-subtitle>
 
-         <v-card-subtitle>created on {{ item.date | formatDate }}</v-card-subtitle>
-         <v-divider></v-divider>
-        <v-card-text class="pa-3" >
+          <v-card-text class="">
+            <carousel
+              :per-page="1"
+              autoplay
+              centerMode
+              loop
+              :autoplayTimeout="randomTimer()"
+              :speed="3000"
+            >
+              <slide>
+                <div class="subtitle-2">
+                  {{ item.description }}
+                </div>
+              </slide>
+              <slide>
+                <v-row justify="center" align="center">
+                  <v-img
+                    :max-width="300"
+                    src="https://github.com/getJv/biolotus-site/blob/master/.github/preview.gif?raw=true"
+                  />
+                </v-row>
+              </slide>
+            </carousel>
+          </v-card-text>
 
-          <div class="subtitle-2">
-            {{ item.description }}
-          </div>
+          <v-card-actions>
+            <v-btn small :color="item.color" outlined :href="item.url">
+              Repository
+            </v-btn>
 
+            <v-spacer></v-spacer>
 
-        </v-card-text>
-        <v-card-actions>
-          <v-btn
-            outlined
-            :href="item.url"
-          >
-            Go Repository
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-timeline-item>
-  </v-timeline>
+            <v-btn small :color="item.color" outlined :href="item.url">
+              Live Preview
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-timeline-item>
+    </v-timeline>
+  </div>
 </template>
 
 <script>
-import axios from 'axios'
-import moment from 'moment';
+import axios from "axios";
+import moment from "moment";
+import { Carousel, Slide } from "vue-carousel";
 
-  export default {
-    created(){
-      this.getData()
+export default {
+  components: {
+    Carousel,
+    Slide
+  },
+  created() {
+    this.getGitData();
+  },
+  computed: {
+    loading() {
+      return this.gitLoading;
+    }
+  },
+  methods: {
+    randomTimer() {
+      return Math.floor(Math.random() * 3000 + 3000);
     },
-    methods:{
-      async getData(){
-        var { data } = await axios.get("https://api.github.com/users/getjv/repos?per_page=100&type=owner");
+    async getGitData() {
+      var { data } = await axios.get(
+        "https://api.github.com/users/getjv/repos?per_page=100&type=owner"
+      );
 
-         data.forEach(item => {
-          var  splitedDescription = item.description ? item.description.split('||') : []
-          if(splitedDescription.length > 1){
-            this.items.push( {
-              type: 'portfolio',
-              color: 'purple darken-1',
-              icon: 'mdi-github',
-              name: item.name,
-              description: splitedDescription[0],
-              url: item.html_url,
-              date: item.created_at,
-              tags: splitedDescription[1].toLowerCase().replace(/ /g,'').split(';')
-            });
+      data.forEach(item => {
+        var splitedDescription = item.description
+          ? item.description.split("||")
+          : [];
+        if (splitedDescription.length > 1) {
+          this.items.push({
+            type: "portfolio",
+            color: "purple darken-1",
+            icon: "mdi-github",
+            name: item.name,
+            description: splitedDescription[0],
+            url: item.html_url,
+            date: item.created_at,
+            tags: splitedDescription[1]
+              .toLowerCase()
+              .replace(/ /g, "")
+              .split(";")
+          });
+        }
+      });
 
-          }
-
-
-        });
-
-        //console.log(result)
-        return data;
-      }
-    },
-    data: () => ({
-      items: [],
-    }),
-    filters:{
-      formatDate(value){
-        return moment(String(value)).format('YYYY-MM-DD hh:mm')
-      }
+      //console.log(result)
+      this.gitLoading = false;
+      return data;
+    }
+  },
+  data: () => ({
+    items: [],
+    gitLoading: true
+  }),
+  filters: {
+    formatDate(value) {
+      return moment(String(value)).format("YYYY-MM-DD hh:mm");
     }
   }
+};
 </script>
-

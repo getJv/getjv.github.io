afaa444aed63908b3ba1b0d30bfb4068663d2149
Jhonatan Morais <jhonatan.morais@gmail.com>
2020-11-01 02:26:54 -0300

    new release

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,77 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-row justify="center" align="center" style="height:100vh">
+    <v-card>
+      <center class="mt-n15">
+        <img class="text--center" alt="Jhonatan Morais photo" :src="image" />
+      </center>
+      <v-card-text class="pa-8">
+        <div class="text-center display-1 ">
+          Jhonatan Morais
+        </div>
+        <div class="text-center title ">
+          Full Stack developer
+        </div>
+        <div class="text-center caption mb-7  ">
+          Laravel | Springboot | VueJS | Flutter
+        </div>
+        <div class="text-center">
+          <v-btn
+            v-for="(item, i) in items"
+            :key="i"
+            :href="item.path"
+            icon
+            class="mx-2 "
+          >
+            <v-icon v-text="item.icon" :color="item.color"></v-icon>
+          </v-btn>
+        </div>
+        <div class="text-center mt-10 ">
+          <v-btn color="blue darken-1" outlined to="/portfolio">
+            Portfolio
+          </v-btn>
+        </div>
+      </v-card-text>
+    </v-card>
+  </v-row>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import jhonatanImage from "@/assets/jhonatan-circle-cropped.png";
 export default {
   name: "Home",
-  components: {
-    HelloWorld
-  }
+  computed: {
+    image() {
+      return jhonatanImage;
+    },
+
+    items() {
+      return [
+        {
+          icon: "mdi-github",
+          text: "GitHub",
+          path: "https://github.com/getJv",
+          color: "blue darken-1",
+        },
+        {
+          icon: "mdi-linkedin",
+          text: "Linkedin",
+          path: "https://www.linkedin.com/in/jhonatan-morais/?locale=en_US",
+          color: "blue darken-3",
+        },
+        {
+          icon: "mdi-twitter",
+          text: "Twitter",
+          path: "https://twitter.com/getjv",
+          color: "blue lighten-3",
+        },
+        {
+          icon: "mdi-pdf-box",
+          text: "Resume",
+          path: "",
+          color: "red darken-3",
+        },
+      ];
+    },
+  },
 };
 </script>
